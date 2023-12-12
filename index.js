e3a34c2c7c62db4f75407c344489767dde20daba
siomcgill15 <151243978+siomcgill15@users.noreply.github.com>
2023-12-12 19:51:55 +0000

    Amended html lines 50 + 55

--- index.js
+++ index.js
@@ -47,12 +47,12 @@ function searchCity(city) {
 
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
+  let searchInput = document.querySelector(".search-form-input");
 
   searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
+let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
